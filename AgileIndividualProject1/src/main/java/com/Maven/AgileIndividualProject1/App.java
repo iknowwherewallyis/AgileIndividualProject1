4024934f14461d61d076236b52e3d1b50e89aa56
John Handy <jhandy92@gmail.com>
2017-02-02 13:20:26 +0000

    Testing Travis

--- AgileIndividualProject1/src/main/java/com/Maven/AgileIndividualProject1/App.java
+++ AgileIndividualProject1/src/main/java/com/Maven/AgileIndividualProject1/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Testing the Travis CI build with a new push" );
     }
 }
