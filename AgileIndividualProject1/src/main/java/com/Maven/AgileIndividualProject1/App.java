e7d7f7e4ed9baaffc5a4d830ff36f0bdc38d4df8
John Handy <jhandy92@gmail.com>
2017-02-02 10:51:07 +0000

    Initial Commit

---
+++ AgileIndividualProject1/src/main/java/com/Maven/AgileIndividualProject1/App.java
@@ -0,0 +1,13 @@
+package com.Maven.AgileIndividualProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
